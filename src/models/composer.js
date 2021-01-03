28af963e826daf4a9b19a14c31c72209889840c8
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-03 14:14:35 -0500

    created inheritable ExpandableSchema for models with default expand and toClient methods

--- src/models/composer.js
+++ src/models/composer.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
-const { Schema } = mongoose
 const { InfoSchema } = require('./info')
-const { generateId, List } = require('./')
+const { ExpandableSchema } = require('./')
 
-const ComposerSchema = new Schema({
-  _id: {
-    type: String,
-    default: generateId
-  },
+const MODEL_NAME = 'composer'
+
+const ComposerSchema = ExpandableSchema(MODEL_NAME, {
   name: {
     title: String,
     first: {type: String, required: true},
@@ -18,74 +15,14 @@ const ComposerSchema = new Schema({
   },
   info: InfoSchema,
   catalog: String
-})
-
-ComposerSchema.virtual('works', {
-  ref: 'work',
-  localField: '_id',
-  foreignField: 'composer',
-});
-
-// TODO: only supports first-level expansions; should support nested expansion
-// TODO: cleanup expand algorithm
-const EXPANDABLE_DOCS = []
-const EXPANDABLE_ARRAYS = ['works']
-const POPULATE_LIMIT = 10
-
-ComposerSchema.method('expand', async function(props) {
-  let query = this
-
-  // TODO: protect against duplicate props
-
-  props.forEach(prop => {
-    const isDoc = EXPANDABLE_DOCS.includes(prop)
-    const isArray = EXPANDABLE_ARRAYS.includes(prop)
-
-    if (!isDoc && !isArray) throw new Error(`Unable to expand property ${prop} in composer.`) // TODO: throw error catchable by API route to throw a 400
-    
-    if (isArray) {
-      query.populate({path: prop, options: {limit: POPULATE_LIMIT}})
-    } else {
-      query = query.populate(prop)
-    }
-  })
-
-  let result = await query.execPopulate()
-
-  props.forEach(prop => {
-    if (EXPANDABLE_ARRAYS.includes(prop)) {
-      result.$locals[prop] = {hasMore: result[prop].length >= POPULATE_LIMIT}
-    }
-  })
-
-  return result
-})
-
-// TODO: generalize using EXPANDABLE_PROPS
-ComposerSchema.method('toClient', function() {
-  let obj = this.toObject()
-
-  const id = obj._id
-  const object = 'composer'
-
-  if (obj.info) obj.info = this.info.toClient()
-  if (this.works) {
-    const data = this.works.map(w => w.toClient())
-    const list = new List(data, this.$locals.works.hasMore, '/') // TODO: use real url
-
-    obj.works = list.toClient()
+}, {
+  works: {
+    ref: 'work',
+    localField: '_id',
+    foreignField: 'composer',
   }
-
-  const prune = ({name, info, catalog, works}) => ({id, object, name, info, catalog, works})
-  obj = prune(obj)
-
-  if (this.$locals.deleted) {
-    obj.deleted = true
-  }
-
-  return obj
 })
 
-const Composer = mongoose.model('composer', ComposerSchema)
+const Composer = mongoose.model(MODEL_NAME, ComposerSchema)
 
 module.exports = {Composer, ComposerSchema}
--- src/models/work.js
+++ src/models/work.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose')
-const { generateId } = require('./')
-const { Schema } = mongoose
+const { ExpandableSchema } = require('./')
 const { InfoSchema } = require('./info')
 const { MovementSchema } = require('./movement')
 
-const WorkSchema = new Schema({
-  _id: {
-    type: String,
-    default: generateId
-  },
+const MODEL_NAME = 'work'
+
+const WorkSchema = ExpandableSchema('work', {
   composer: {
     type: String,
     required: true,
@@ -23,28 +20,6 @@ const WorkSchema = new Schema({
   info: InfoSchema
 })
 
-WorkSchema.method('toClient', function() {
-  let obj = this.toObject()
-
-  const id = obj._id
-  const object = 'work'
-
-  if (obj.info) obj.info = this.info.toClient()
-  if (obj.movements) obj.movements = this.movements.map(m => m.toClient())
-  if (this.populated('composer')) obj.composer = this.composer.toClient()
-
-  const prune = ({composer, name, movements, catalog, info}) =>
-    ({id, object, composer, name, movements, catalog, info})
-  
-  obj = prune(obj)
-
-  if (this.$locals.deleted) {
-    obj.deleted = true
-  }
-
-  return obj
-})
-
-const Work = mongoose.model('work', WorkSchema)
+const Work = mongoose.model(MODEL_NAME, WorkSchema)
 
 module.exports = {Work, WorkSchema}
