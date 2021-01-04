e9ba87024e235870767e0410247ee402a43393ab
Hyrum Cooper <hyrumcoop@gmail.com>
2021-01-04 09:12:57 -0500

    performer routes

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ const router = require('express-promise-router')()
 const composer = require('./routes/composer')
 const work = require('./routes/work')
 const performance = require('./routes/performance')
+const performer = require('./routes/performer')
 
 const { errorHandler, NotFoundError } = require('./middleware/errors')
 
@@ -9,6 +10,7 @@ const routes = () => {
   composer(router)
   work(router)
   performance(router)
+  performer(router)
 
   // return 404 for undefined routes
   router.use('*', () => {
---
+++ src/api/routes/performer.js
@@ -0,0 +1,50 @@
+const route = require('express-promise-router')()
+const { ListSchema, PostSchema, PatchSchema } = require('../schemas/performer')
+const PerformerService = require('../../services/performer')
+const { NotFoundError } = require('../middleware/errors')
+const { bodyValidator, queryValidator } = require('../middleware/validation')
+
+module.exports = (router) => {
+  router.use('/performer', route)
+
+  route.get('/', queryValidator(ListSchema), async (req, res) => {
+    const {limit, start} = req.query
+
+    const components = await PerformerService.list({limit, skip: start})
+
+    res.sendDocument(components)
+  })
+
+  route.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const performer = await PerformerService.findById(id)
+
+    if (!performer) throw new NotFoundError(`Performer with id '${id}' does not exist.`)
+
+    res.sendDocument(performer)
+  })
+
+  route.post('/', bodyValidator(PostSchema), async (req, res) => {
+    const performer = await PerformerService.create(req.body)
+
+    res.sendDocument(performer)
+  })
+
+  route.patch('/:id', bodyValidator(PatchSchema), async (req, res) => {
+    const id = req.params.id
+    const performer = await PerformerService.update(id, req.body)
+
+    if (!performer) throw new NotFoundError(`Performer with id ${id} does not exist.`)
+
+    res.sendDocument(performer)
+  })
+
+  route.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    const performer = await PerformerService.delete(id)
+
+    if (!performer) throw new NotFoundError(`Performer with id '${id}' does not exist.`)
+
+    res.sendDocument(performer)
+  })
+}
